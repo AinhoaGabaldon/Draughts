82b3bfbe4378261e8b0ca8ec2e67f278777e2185
Ainhoa Gabaldon <457183.clot@fje.edu>
2025-10-13 13:24:30 +0200

    Create and print board

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
-// Draugths.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 
 #include <iostream>
+#include <vector>
+
+std::vector<char> board {
+        '.', 'X', '.', 'X', '.', 'X', '.', 'X',
+        'X', '.', 'X', '.', 'X', '.', 'X', '.',
+        '.', 'X', '.', 'X', '.', 'X', '.', 'X',
+        '.', '.', '.', '.', '.', '.', '.', '.',
+        '.', '.', '.', '.', '.', '.', '.', '.',
+        'O', '.', 'O', '.', 'O', '.', 'O', '.',
+        '.', 'O', '.', 'O', '.', 'O', '.', 'O',
+        'O', '.', 'O', '.', 'O', '.', 'O', '.',
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << "My name is Ainhoa\n";
+    for (int i = 0; i < board.size(); ++i) {
+        if (i % 8 == 0)
+             std::cout << '\n';
+
+        std::cout << ' ' << board.at(i);
+    }
+
+    std::cout << "\n\n";
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
